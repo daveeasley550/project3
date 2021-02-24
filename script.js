1112fcc2508c35eb94cee5d4fb14738294a7f7c4
Dave Easley <easley.david@gmail.com>
2021-02-24 15:40:57 -0500

    completed

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const randomUrl = "http://api.icndb.com/jokes/random/"
-// const url = "http://api.icndb.com/"
 let randomJoke = document.querySelector("p")
-// console.log(randomJoke)
 let randomButton = document.querySelector(".randomButton")
 let nextButton = document.querySelector(".nextButton")
 let previousButton = document.querySelector(".previousButton")
@@ -10,59 +8,48 @@ let counter = 0
 let joke = function (data) {
     randomJoke.innerHTML = `${data}`
 }
-for (let i = 0; i < 40; i++) {
-
-    fetch(randomUrl)
-        .then(response => response.json())
-        .then(data => {
-            oldJokes.push(data.value.joke)
-            randomJoke.innerHTML = oldJokes[40]
-        })
+let toggleButton = function () {
+    if (counter == 0) {
+        previousButton.style.display = "none";
+    } else previousButton.style.display = "";
 }
+toggleButton()
 nextButton.addEventListener("click", function (e) {
     e.preventDefault()
-    randomJoke.innerHTML = oldJokes[counter += 1]
+    if (counter >= oldJokes.length || counter == 0) {
+        fetch(randomUrl)
+            .then(response => response.json())
+            .then(data => {
+                oldJokes.push(data.value.joke)
+                joke(data.value.joke)
+                counter++
+            })
+        toggleButton()
+    }
+    else if (counter < oldJokes.length) {
+        randomJoke.innerHTML = oldJokes[counter]
+        counter++
+    }
 })
 previousButton.addEventListener("click", function (e) {
     e.preventDefault()
-    randomJoke.innerHTML = oldJokes[counter -= 1]
+    counter--
+    if (counter == 0) {
+        previousButton.style.display = "none"
+    } else {
+        randomJoke.innerHTML = oldJokes[counter - 1]
+    }
 })
 randomButton.addEventListener("click", function (e) {
     e.preventDefault()
     fetch(randomUrl)
         .then(response => response.json())
-        .then(data => joke(data.value.joke))
+        .then(data => {
+            oldJokes.push(data.value.joke)
+            joke(data.value.joke)
+            counter++
+        })
+    toggleButton()
 })
-// if()
-// console.log(oldJokes)
-randomButton.click()
 
 
-
-
-
-// const jsGallery = document.querySelector(".js-gallery")
-// const jsGalleryItems = document.querySelectorAll(".js-gallery-item")
-
-
-
-// let slideCount = jsGalleryItems.length
-// let slideWidth = 1000
-// let intervalId = window.setInterval(transitionSlide, 5000)
-
-//use this for each button to change it, look at gom for click event listener and button
-// let currentSlide = 1;
-
-// function transitionSlide(){
-// if(currentSlide < slideCount){
-//   let delta = currentSlide*slideWidth*-1
-// jsGallery.style.transform = `translateX(${delta}px)`
-// currentSlide++
-// console.log(currentSlide)
-// }
-// else{
-  // console.log(currentSlide)
-//   jsGallery.style.transform = "translateX(0)"
-//   currentSlide = 1
-// }
-// }
